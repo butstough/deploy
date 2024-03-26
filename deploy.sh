60d6495e0e4b8d478c6d4b529aada98b31397202
butstough <90295936+butstough@users.noreply.github.com>
2024-03-26 05:09:22 -0700

    reordered to install login apps first
    reordered install process so that discord (and other apps requiring sign-in) can be set up during the build/install process of other apps

--- deploy.sh
+++ deploy.sh
@@ -17,17 +17,39 @@ sudo chown root:root /etc/bash_completion.d/apt-fast
 
 source ~/.profile
 
+# install standalone deb apps
 
+# discord
+wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+sudo dpkg -i discord.deb
+
+# add repos
+
+# multiverse (needed for steam)
+sudo add-apt-repository -y multiverse
+
+# firefox nightly
+sudo add-apt-repository -y ppa:ubuntu-mozilla-daily/ppa
+
+# musicbrainz
+sudo add-apt-repository -y ppa:musicbrainz-developers/stable
+
+# update system and install new software through apt
 sudo apt update
 sudo apt-fast -y upgrade
 sudo apt autoremove -y
+sudo apt-fast -y install --install-recommends firefox-trunk steam picard winehq-stable vim
 
-
+# build and install ffmpeg
 cd ~/deploy/ffmpeg
 ./build.sh
 
+# obs studio
+sudo add-apt-repository -y ppa:obsproject/obs-studio
+# install obs
+sudo apt update
+sudo apt-fast -y install --install-recommends obs-studio
 
-# add repos
 
 # enable 32 bit
 sudo dpkg --add-architecture i386
@@ -36,35 +58,4 @@ sudo dpkg --add-architecture i386
 sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key "https://dl.winehq.org/wine-builds/winehq.key"
 sudo wget -NP /etc/apt/sources.list.d/ "https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources"
-
-
-# multiverse (needed for steam)
-sudo add-apt-repository -y multiverse
-
-# obs studio
-sudo add-apt-repository -y ppa:obsproject/obs-studio
-
-# firefox nightly
-sudo add-apt-repository -y ppa:ubuntu-mozilla-daily/ppa
-
-# musicbrainz
-sudo add-apt-repository -y ppa:musicbrainz-developers/stable
-
-# install new apps from repos
-sudo apt update
-sudo apt-fast -y install --install-recommends obs-studio firefox-trunk steam picard winehq-stable vim
-
-
-# install standalone deb apps
-
-# discord
-wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
-sudo dpkg -i discord.deb
-
-
-
-
-
-
-
 
